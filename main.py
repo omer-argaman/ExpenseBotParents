06923dbae6e9ddec70611c1c441efec1de64f112
omer-argaman <omer.argaman@post.runi.ac.il>
2025-11-16 13:16:43 +0200

    improved mapping for parents

--- main.py
+++ main.py
@@ -123,66 +123,7 @@ if service is None:
 
 # Category mapping
 category_map = {
-    # Transportation
-    "Public Transportation": [
-        "train", "taxi", "bus", "metro", "tram", "cab", "subway", "ride",
-        "public", "transport"
-    ],
-    "Fuel Toyota": ["Fuel Toyota", "fuel toyota", "diesel"],
-    "Fuel MG": ["Fuel MG", "petrol", "fuel mg"],
-    "Parking": ["parking", "garage", "space", "charging", "park"],
-    "Other (Trans)": [
-        "transportation other", "other transportation", "Other (Trans)",
-        "Other Trans", "Other Transportation"
-    ],
 
-    # Home Expenses
-    "Rent": ["rent", "lease", "apartment fee", "monthly payment", "mortgage"],
-    "Electricity": ["electricity", "electric", "power", "energy", "bill", "utility"],
-    "Gas": ["gas", "oil", "heating",],
-    "Water":
-    ["water", "sewer", "trash", "garbage", "utilities", "waste"],
-    "Property Tax": ["Property Tax", "Tax", "Arnona"],
-    "Internet": ["internet", "wifi", "broadband", "net", "phone"],
-    "House Committee":
-    ["committee", "house committee", "hoa", "maintenance fee"],
-    "Maintenance":
-    ["maintenance", "improvement", "repair", "fix", "home", "flowers", "flower", "upgrade", "repairs"],
-
-    # Daily Living
-    "Groceries":
-    ["groceries", "supermarket", "market", "super", "store", "shopping", "food", "dani" , "danny"],
-    "Coffee": [
-        "coffee", "latte", "espresso", "cappuccino", "brew", "americano",
-        "mocha", "cafe"
-    ],
-    "Dining Out": [
-        "dining", "restaurant", "meal", "food", "breakfast", "lunch", "dinner", "eat",
-        "takeout", "delivery"
-    ],
-    "Beer / Wine": [
-        "beer", "wine", "alcohol", "bar", "cocktail", "drink", "vodka",
-        "whiskey", "liquor"
-    ],
-    "Cloths": [ "cloths", "shirt", "pants", "dress", "clothes", "cloth", "tshirt", "t-shirt", "t shirt"
-    ],
-    "Pharm": ["Pharm", "pharm", "superpharm" "pharm", "super pharm", "super-pharm", "pharmacy"
-    ],
-    "Other (Daily)": [
-        "daily living other", "other daily living", "haircut",
-        "miscellaneous living", "Other (Daily)", "Other Daily", "other", "cosmetics", "laser",
-        "personal", "gym", "personal care", "present", "presents"
-    ],
-
-    # Entertainment and Recreation
-    "Entertainment": [
-        "entertainment", "movie", "theater", "show", "concert", "game",
-        "festival", "fun", "games"
-    ],
-    "Vacation": [
-        "vacation", "holiday", "trip", "travel", "hotel", "flight", "beach",
-        "resort"
-    ],
 
     # Education and Healthcare
     "Education": ["books", "courses", "education", "school"],
@@ -192,13 +133,88 @@ category_map = {
         "medical", "healthcare"
     ],
 
-    # Savings and Insurance
-    "Life Insurance": ["life insurance", "policy", "premium", "coverage", "car insurance", "health insurance"],
-    "Emergency Fund": ["emergency fund", "savings", "rainy day"],
-    
     # Personal Categories
-    "Omer": ["omer"],
-    "Gil": ["gil"],
+    "Mom": ["Mom", "Ilanit", "Business", "Sapakim" ],
+    "Dad": ["Hafenix", "Bitoach Leumi", "BLL"],
+    "Ramat Gan": ["Noy", "Avigdor", "Yigal"],
+    "Extra": ["Extra", "Bonus"],
+
+    # Savings and Insurance
+    "Emergency Fund": ["emergency fund", "savings", "rainy day"],
+    "Kupat Gemel": ["Gemel", "Kupat Gemel", "Kupat Gemel Mom", "Mom Kupat Gemel"],
+    "Savings": ["Savings", "Saveings", "IBI"],
+
+    # Home Expenses
+    "Mortage": ["mortgage"],
+    "Electricity": ["electricity", "electric", "energy", "utility", "hashmal"],
+    "Gas": ["gas", "gas home"],
+    "Water":["water", "sewer", "trash", "garbage", "utilities", "waste"],
+    "Property Tax": ["Property Tax", "Tax", "Arnona"],
+    "Internet": ["internet", "wifi", "broadband", "net",],
+    "TV":["TV"],
+    "Cellular":["Cellular", "Phone", "Pelephone", "phone bill", "Telephone"],
+    "Gardner":["Gardner"],
+    "Heating":["Heating", "Oil"],
+    "Elza":["Elza food", "Elza", "Vet", "Veterinar", "Vetrinar", "Dog", "Dog Food"],
+    "Cleaner":["Cleaner", "House Keeper", "House Keeping"],
+    "Maintenance":["maintenance", "improvement", "repair", "fix", "home", "flowers", "flower", "upgrade", "repairs"],
+
+    # Transportation
+    "Fuel Toyota": ["Fuel Toyota", "fuel toyota", "diesel", "gas toyota"],
+    "Fuel MG": ["Fuel MG", "petrol", "fuel mg", "gas MG"],
+    "Road Talls": ["Road Talls", "Talls", "Road six", "Kvish shesh", "Kvish", "Road"],
+    "Public Transportation": ["train", "taxi", "bus", "metro", "tram", "cab", "subway", "ride", "public", "transport"],
+    "Parking": ["parking", "garage", "space", "charging", "park", "pango", "cello", "cellopark"],
+    "Car Insurance Toyota": ["Car Insurance Toyota", "Insurance toyota"],
+    "Car Insurance MG": ["Car Insurance MG", "Insurance MG"],
+    "Maintenance Toyota": ["Test Toyota", "Tipul toyota", "Musah toyota", "Garage toyota", "Mechanic toyota"],
+    "Maintenance MG": ["Test MG", "Tipul MG", "Musah mg", "Garage MG", "Mechanic MG"],
+    "Other (Trans)": ["transportation other", "other transportation", "Other (Trans)",
+        "Other Trans", "Other Transportation"],
+    
+    #Insurance
+    "Life": ["life insurance", "life insurance Dad", "life insurance Mom", "life insurance Risk",],
+    "Nursing": ["Nursing insurance", "Nursing",],
+    "Structure": ["Structure insurance", "Structure"],
+    "Health Care": ["Health insurance", "Clalit", "Clalit Shahar", "Clalit Ilanit", "Clalit Nadav", "Clalit Omer",
+        "Clalit Alon", "Clalit Naama", "Siudi Clalit Shahar", "Siudi Clalit Ilanit", "Siudi Clalit Nadav", "Siudi Clalit Omer",
+        "Siudi Clalit Alon", "Siudi Clalit Naama" "Siudi Shahar", "Siudi Ilanit", "Siudi Nadav", "Siudi Omer",
+        "Siudi Alon", "Siudi Naama", "Siudi"],
+    "Mortgage": ["Mortgage insurance",],
+    "Health Insurance": ["Health Insurance"],
+
+    # Daily Living
+    "Groceries":
+    ["groceries", "supermarket", "market", "super", "store", "shopping", "food", "dani" , "danny"],
+    "Vitamins": ["Vitamins", "Omega three", "Vitamin", "Omega"],
+    "Dining Out": ["dining", "restaurant", "meal", "food", "breakfast", "lunch", "dinner", "eat","takeout", "delivery"
+    ],
+    "Beer / Wine": ["beer", "wine", "alcohol", "bar", "cocktail", "drink", "vodka",
+        "whiskey", "liquor"],
+    "Other (Daily)": ["daily living other", "other daily living", "miscellaneous living", "Other (Daily)", "Other Daily", "other", "cosmetics", "laser",
+        "personal", "gym", "present", "presents"
+    ],
+    "Cloths": [ "cloths", "shirt", "pants", "dress", "clothes", "cloth", "tshirt", "t-shirt", "t shirt"],
+    "Kids": [ "Naama", "Alon", "Nadav", "Omer", "Naama tzofim", "tzofim", "Naama Studio", "Studio", 
+        "Naama Driving Licence", "Driving Licence", "Driving Lessons", "Driving Lesson", "Matnas"],
+    "Education": ["School", "Naama school", "Tel Aviv Univercity", "Uni", "Univercity", "Education", "Edu",
+        "Private lesson", "Privet Lesson", "Shiur prati", "Lesson", "Shiur"],
+    "Entertainment": ["entertainment", "movie", "Movies", "theater", "show", "concert", "game", "festival", "fun", "games"
+        "Haaretz", "News Paper", "News", "newspaper", "Haarets"],
+    "Vacation": ["vacation", "holiday", "trip", "travel", "hotel", "flight", "beach", "resort", "Jip Trip",
+        "Jeep trip", "Jeep trips"],
+    "Pharm": ["Pharm", "pharm", "superpharm" "pharm", "super pharm", "super-pharm", "pharmacy"],
+    "Beauty": ["Beauty", "Tipuah", "Haircut", "Hair cut", "Ilana", "Nails", "Hair Color", "personal care"],
+    "Treatment": ["Treatment", "RINA", "Rina", "pool", "Alon Rosenberg", "Rosenberg", "Tipul"],
+    
+    # Business
+    "Contructors": ["Contructors"],
+    "Software": ["Application", "Software", "Autocad", "Autodesk"],
+    "Insurance": [],
+    "Office Appliance": ["office appliance", "Office", "Misrad"],
+    "Accountant": ["Accountant", "Accounting", "Ziv Shifer", "Ziv"],
+    "Other (Business)": ["Other Business", "Other esek", "Esek"],
+
 }
 
 # Broad Category Definitions mapping main categories to their subcategories
